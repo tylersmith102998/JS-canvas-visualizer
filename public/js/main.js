7cb50d7de37a11c7e70b4fa2930f3f26d1904efa
Tyler Smith <tyler.smith102998@gmail.com>
2023-11-25 15:44:29 -0600

    Node space simulation

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,11 @@ var debug = false;
 //var debug = true;
 
 // create a new engine
-const engine = new Engine({ FPSTarget: 60 });
+const engine = new Engine({
+	FPSTarget: 60,
+	backgroundColor: [1, 1, 15, 1],
+	debug: debug,
+});
 
 // create a new node
 //const node = new Node(engine, 50, 100, 10, "red", debug);
@@ -17,19 +21,31 @@ const engine = new Engine({ FPSTarget: 60 });
 // array of nodes
 const nodes = [];
 
-// create 10 nodes with random coords from -1000 to 1000
-for (let i = 0; i < 10; i++) {
+// create 1 node
+nodes.push(new Node(engine, 0, 0, 5, "red", debug));
+
+let medianX = 0;
+let medianY = 0;
+
+// create 1 node every 5 seconds
+setInterval(() => {
+	// get random r, g, b values
+	const r = Math.floor(Math.random() * 255);
+	const g = Math.floor(Math.random() * 255);
+	const b = Math.floor(Math.random() * 255);
+
+	// random radius between 5 and 30
+	const radius = Math.random() * 25 + 5;
+
 	nodes.push(
-		new Node(
-			engine,
-			Math.random() * 2000 - 1000,
-			Math.random() * 2000 - 1000,
-			10,
-			"red",
-			debug
-		)
+		new Node(engine, medianX, medianY, radius, `rgb(${r}, ${g}, ${b})`, debug)
 	);
-}
+
+	// remove nodes if there are too many
+	if (nodes.length > 200) {
+		nodes.shift();
+	}
+}, 1000);
 
 // update canvas size on window resize
 window.addEventListener("resize", () => {
@@ -69,16 +85,43 @@ function update(engine) {
 	});
 
 	// get a random node
-	const randomNode = nodes[Math.floor(Math.random() * nodes.length)];
+	const randomNode =
+		nodes[Math.floor(Math.random() * Math.floor(nodes.length))];
 
-	// update radius
-	//randomNode.radius = Math.random() * 20 + 10;
+	// multiply radius by random number between 0.9 and 1.1
+	//randomNode.radius *= Math.random() * 0.2 + 0.9;
+
+	// if radius is less than 1, set to 1
+	if (randomNode.radius < 1) {
+		randomNode.radius = 1;
+	} else if (randomNode.radius > 30) {
+		randomNode.radius = 30;
+	}
+
+	// get median x and y
+
+	nodes.forEach((node) => {
+		medianX += node.position.x;
+		medianY += node.position.y;
+	});
+	medianX /= nodes.length;
+	medianY /= nodes.length;
 
 	// update camera
-	engine.camera.update(nodes[0].position);
+	//engine.camera.update(nodes[0].position);
+	engine.camera.update(new Vector2D(medianX, medianY));
 	//engine.camera.update(
 	//	new Vector2D(engine.canvas.width / 2, engine.canvas.height / 2)
 	//);
+
+	nodes.forEach((node) => {
+		if (Math.random() < 0.01) {
+			node.velocity = new Vector2D(
+				Math.random() * 200 - 100,
+				Math.random() * 200 - 100
+			);
+		}
+	});
 }
 
 function draw(engine) {
@@ -86,6 +129,57 @@ function draw(engine) {
 	nodes.forEach((node) => {
 		node.draw(engine.context);
 	});
+
+	// debug
+	if (true) {
+		// draw median x and y
+		engine.context.fillStyle = "white";
+		engine.context.font = "12px Arial";
+		engine.context.textAlign = "left";
+
+		// get total node count on screen
+		let nodeCount = 0;
+		nodes.forEach((node) => {
+			if (
+				engine.camera.getOffsetX(node.position.x) + node.radius > 0 &&
+				engine.camera.getOffsetX(node.position.x) - node.radius <
+					engine.canvas.width &&
+				engine.camera.getOffsetY(node.position.y) + node.radius > 0 &&
+				engine.camera.getOffsetY(node.position.y) - node.radius <
+					engine.canvas.height
+			) {
+				nodeCount++;
+			}
+		});
+
+		// get tracer count from each node
+		var tracerCount = 0;
+		nodes.forEach((node) => {
+			if (
+				engine.camera.getOffsetX(node.position.x) + node.radius > 0 &&
+				engine.camera.getOffsetX(node.position.x) - node.radius <
+					engine.canvas.width &&
+				engine.camera.getOffsetY(node.position.y) + node.radius > 0 &&
+				engine.camera.getOffsetY(node.position.y) - node.radius <
+					engine.canvas.height
+			) {
+				node.tracerPositions.forEach((tracer) => {
+					if (
+						engine.camera.getOffsetX(tracer.position.x) > 0 &&
+						engine.camera.getOffsetX(tracer.position.x) < engine.canvas.width &&
+						engine.camera.getOffsetY(tracer.position.y) > 0 &&
+						engine.camera.getOffsetY(tracer.position.y) < engine.canvas.height
+					) {
+						tracerCount++;
+					}
+				});
+			}
+		});
+
+		engine.context.fillText("Node Count: " + nodeCount, 10, 40);
+
+		engine.context.fillText("Tracer Count: " + tracerCount, 10, 60);
+	}
 }
 
 engine.start(update, draw);
--- public/js/src/Engine.js
+++ public/js/src/Engine.js
@@ -3,122 +3,138 @@ import FollowCamera from "./FollowCamera.js";
 // Custom engine for the game
 
 const defaultOptions = {
-    canvas: "canvas",
-    width: window.innerWidth,
-    height: window.innerHeight,
-    backgroundColor: [20, 20, 20, 1],
-    FPSTarget: 60,
-    showFPS: true,
+	canvas: "canvas",
+	width: window.innerWidth,
+	height: window.innerHeight,
+	backgroundColor: [20, 20, 20, 1],
+	FPSTarget: 60,
+	showFPS: true,
+	debug: false,
 };
 
 export default class Engine {
-    // constructor
-    constructor(options = {}) {
-        // merge options with default options
-        this.options = Object.assign({}, defaultOptions, options);
+	// constructor
+	constructor(options = {}) {
+		// merge options with default options
+		this.options = Object.assign({}, defaultOptions, options);
 
-        // set the game loop
-        this.gameLoop = null;
+		// set the game loop
+		this.gameLoop = null;
 
-        // check if backgroundColor is array, and if so, convert it to rgba
-        if (Array.isArray(this.options.backgroundColor)) {
-            this.options.backgroundColor =
-                "rgba(" + this.options.backgroundColor.join(",") + ")";
-        }
+		// check if backgroundColor is array, and if so, convert it to rgba
+		if (Array.isArray(this.options.backgroundColor)) {
+			this.options.backgroundColor =
+				"rgba(" + this.options.backgroundColor.join(",") + ")";
+		}
 
-        console.log(this.options);
+		console.log(this.options);
 
-        // get the canvas
-        this.canvas = document.getElementById(this.options.canvas);
+		// get the canvas
+		this.canvas = document.getElementById(this.options.canvas);
 
-        // get the context
-        this.context = this.canvas.getContext("2d");
+		// get the context
+		this.context = this.canvas.getContext("2d");
 
-        // set canvas width and height to window size
-        this.canvas.width = this.options.width;
-        this.canvas.height = this.options.height;
+		// set canvas width and height to window size
+		this.canvas.width = this.options.width;
+		this.canvas.height = this.options.height;
 
-        // scale the canvas using css transform
-        //const scaleX = window.innerWidth / this.canvas.width;
-        //const scaleY = window.innerHeight / this.canvas.height;
+		// scale the canvas using css transform
+		//const scaleX = window.innerWidth / this.canvas.width;
+		//const scaleY = window.innerHeight / this.canvas.height;
 
-        //const scaleToFit = Math.min(scaleX, scaleY);
-        //const scaleToCover = Math.max(scaleX, scaleY);
+		//const scaleToFit = Math.min(scaleX, scaleY);
+		//const scaleToCover = Math.max(scaleX, scaleY);
 
-        //this.canvas.style.transformOrigin = "0 0";
-        //this.canvas.style.transform = "scale(" + scaleToFit + ")";
+		//this.canvas.style.transformOrigin = "0 0";
+		//this.canvas.style.transform = "scale(" + scaleToFit + ")";
 
-        this.times = [];
-        this.fps = 0;
+		this.times = [];
+		this.fps = 0;
 
-        // set up the camera
-        this.camera = new FollowCamera(this.context);
-    }
+		// set up the camera
+		this.camera = new FollowCamera(this.context);
+	}
 
-    // game loop
-    loop() {
-        // request another frame
-        window.requestAnimationFrame(this.loop);
+	// game loop
+	loop() {
+		// request another frame
+		window.requestAnimationFrame(this.loop);
 
-        // if enough time has elapsed, draw the next frame
-        if (this.fpsElapsed > this.fpsInterval) {
-            // Get ready for next frame by setting then=now, but also adjust for your
-            // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
-            this.fpsThen = this.fpsNow - (this.fpsElapsed % this.fpsInterval);
+		// if enough time has elapsed, draw the next frame
+		if (this.fpsElapsed > this.fpsInterval) {
+			// Get ready for next frame by setting then=now, but also adjust for your
+			// specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+			this.fpsThen = this.fpsNow - (this.fpsElapsed % this.fpsInterval);
 
-            // update the game
-            this.update(this);
+			// update the game
+			this.update(this);
 
-            // clear the canvas
-            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+			// clear the canvas
+			this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-            // set background color to slightly lighter than black
-            this.context.fillStyle = this.options.backgroundColor;
-            this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+			// set background color to slightly lighter than black
+			this.context.fillStyle = this.options.backgroundColor;
+			this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-            this.gameLoop(this);
+			this.gameLoop(this);
 
-            // output camera position to console
-            //console.log(this.camera.position);
+			// output camera position to console
+			//console.log(this.camera.position);
 
-            // calculate the fps and display it if showFPS is true
-            this.calculateFPS();
-            if (this.options.showFPS) {
-                this.context.fillStyle = "#fff";
-                this.context.font = "12px Arial";
-                this.context.textAlign = "left";
-                this.context.fillText("FPS: " + this.fps, 10, 20);
-            }
-        }
+			// calculate the fps and display it if showFPS is true
+			this.calculateFPS();
+			if (this.options.showFPS) {
+				this.context.fillStyle = "#fff";
+				this.context.font = "12px Arial";
+				this.context.textAlign = "left";
+				this.context.fillText("FPS: " + this.fps, 10, 20);
+			}
 
-        // calculate the time since last frame
-        this.fpsNow = performance.now();
-        this.fpsElapsed = this.fpsNow - this.fpsThen;
-    }
+			if (this.options.debug) {
+				// draw camera center coordinated at bottom left corner
+				this.context.fillStyle = "#fff";
+				this.context.font = "12px Arial";
+				this.context.textAlign = "left";
+				this.context.fillText(
+					"Camera: " +
+						this.camera.position.x.toFixed(2) +
+						", " +
+						this.camera.position.y.toFixed(2),
+					10,
+					this.canvas.height - 10
+				);
+			}
+		}
 
-    // start the engine
-    start(update, gameLoop) {
-        // set the game loop
-        this.gameLoop = gameLoop;
-        this.update = update;
+		// calculate the time since last frame
+		this.fpsNow = performance.now();
+		this.fpsElapsed = this.fpsNow - this.fpsThen;
+	}
 
-        // set FPS target
-        this.fpsInterval = 1000 / this.options.FPSTarget;
-        this.fpsThen = performance.now();
-        this.fpsStartTime = this.fpsThen;
+	// start the engine
+	start(update, gameLoop) {
+		// set the game loop
+		this.gameLoop = gameLoop;
+		this.update = update;
 
-        // start the game loop
-        this.loop = this.loop.bind(this);
-        this.loop();
-    }
+		// set FPS target
+		this.fpsInterval = 1000 / this.options.FPSTarget;
+		this.fpsThen = performance.now();
+		this.fpsStartTime = this.fpsThen;
 
-    // calculate the fps
-    calculateFPS() {
-        var now = performance.now();
-        while (this.times.length > 0 && this.times[0] <= now - 1000) {
-            this.times.shift();
-        }
-        this.times.push(now);
-        this.fps = this.times.length;
-    }
+		// start the game loop
+		this.loop = this.loop.bind(this);
+		this.loop();
+	}
+
+	// calculate the fps
+	calculateFPS() {
+		var now = performance.now();
+		while (this.times.length > 0 && this.times[0] <= now - 1000) {
+			this.times.shift();
+		}
+		this.times.push(now);
+		this.fps = this.times.length;
+	}
 }
